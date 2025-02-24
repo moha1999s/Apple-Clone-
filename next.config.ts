492c141ab6981f5eb17dd837a3f2fdae49bd9347
Mohamed Alaa Eldin <159617907+moha1999s@users.noreply.github.com>
2025-02-24 02:19:23 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-module.exports = {
   reactStrictMode: true,
   experimental: {
-  
+    // ضع خياراتك التجريبية هنا إذا كانت هناك
   },
-}
+  basePath: '/my-project-name', // استبدل '/my-project-name' باسم المشروع الذي ستنشره
+  images: {
+    loader: 'default',
+    path: '/my-project-name/', // المسار الأساسي للصور
+  },
+};
+
 export default nextConfig;
